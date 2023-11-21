be9f7b3aec35ddc35e1950002d84847ef27fbf99
retcal <h.retamales@hotmail.com>
2023-11-21 09:52:39 -0300

    Update age.rb

--- aula 1-2-3/age.rb
+++ aula 1-2-3/age.rb
@@ -3,24 +3,24 @@ result = ''
 loop do
     puts result
     puts 'Selecione una las seguientes opciones:'
-    puts '1 - Calcular la edad de uma persona'
+    puts '1 - Calcular la edad de una persona'
     puts '0 - Salir'
     print 'Opcion: '
 
     option = gets.chomp.to_i
 
     if option == 1
-        print 'Digite o año de nacimento: '
+        print 'Digite el año de nacimento: '
         year_of_birth = gets.chomp.to_i
-        print 'Digite o año actual: '
+        print 'Digite el año actual: '
         current_year = gets.chomp.to_i
         age = current_year - year_of_birth
-        result = "Quem nasceu no ano de #{year_of_birth}, tem #{age} anos em #{current_year} (considerando que a pessoa já tenha aniversariado no ano atual)"
+        result = "Nacido en el año #{year_of_birth}, tiene #{age} años en #{current_year} (considerando que la persona ya ha cumplido años en el año en curso)"
     elsif option == 0
         break
     else
-        result = 'Opção inválida'
+        result = 'Opcion  inválida'
     end
-    # comando que limpa o console
+    # limpia la pantalla
     system "clear"
 end
