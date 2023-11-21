e86e3d7385b3e698a9933d4ce4685bc92c41d681
retcal <h.retamales@hotmail.com>
2023-11-21 10:25:49 -0300

    Update desafio1.rb

--- aula 5/desafio1.rb
+++ aula 5/desafio1.rb
@@ -1,13 +1,13 @@
-#system "clear" #Lembrando que esse comando é apenas para limpar a tela do terminal
+#system "clear" #Recuerde que este comando sólo borra la pantalla del terminal
 
 def calculo(n1, n2)
   result = n1 ** n2
-  puts "O resultado entre #{n1} elevado a #{n2} é #{result}."
+  puts "El resultado entre #{n1} elevado a #{n2} es #{result}."
 end
 
-print "Digite o primeiro numero: "
+print "Digite el primer numero: "
 n1 = gets.chomp.to_i
-print "Digite o segundo numero: "
+print "Digite el segundo numero: "
 n2 = gets.chomp.to_i
 
 calculo(n1, n2)
