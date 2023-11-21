b7cb4d68a9a4b45b0df8474a51e65bbcb015b373
retcal <h.retamales@hotmail.com>
2023-11-21 10:24:31 -0300

    Update method.rb

--- aula 5/method.rb
+++ aula 5/method.rb
@@ -1,28 +1,28 @@
-# métodos são uma forma de organizar instruções em um programa, permitindo que trechos de código sejam reutilizados.
-# sem parâmetros
+# Los métodos son una forma de organizar las instrucciones de un programa, lo que permite reutilizar fragmentos de código.
+# sin parámetros
 def talk
-    puts 'Olá, como você está?'
+    puts 'Hola, ¿cómo estás??'
 
 end
 talk
 
-# com parâmetros
+# con parámetros
 def talk first_name, last_name
-    puts "Olá #{first_name} #{last_name}, como você está?"
+    puts "Hola #{first_name} #{last_name}, cómo te va?"
 end
 
-    first_name = 'Manassés'
-    last_name = 'Almeida'
+    first_name = 'Jose'
+    last_name = 'Torres'
 
     talk(first_name, last_name)
 
-# com parâmetros pré definidos
-def signal(color='vermelho')
-    puts "O sinal está #{color}"
+# con parámetros predefinidos
+def signal(color='rojo')
+    puts "La señal es #{color}"
 end
 
 signal
 
 color = 'verde'
 signal(color)
-signal('amarelo')  
+signal('amarillo')  
