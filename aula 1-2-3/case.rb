cc38507b01ac788565e62f212ce5206167e6e1f6
retcal <h.retamales@hotmail.com>
2023-11-21 10:04:58 -0300

    Update case.rb

--- aula 1-2-3/case.rb
+++ aula 1-2-3/case.rb
@@ -1,15 +1,15 @@
-puts 'Digite o número do mês em que você nasceu:'
+puts 'Introduzca el número del mes en que nació:'
 month = gets.chomp.to_i
 
 case month
 when 1..3
-    puts 'Você nasceu no início do ano!'
+    puts 'Naciste a principios de año!'
 when 9..12
-    puts 'Você nasceu no final do ano!'
+    puts 'Naciste a finales de año!'
 when 4..6
-    puts 'Você nasceu na primeira metade do ano!'
+    puts 'Naciste en la primera mitad del año!'
 when 7..9
-    puts 'Você nasceu na segunda metade do ano!'
+    puts 'Naciste en la segunda mitad del año!'
 else
-    puts 'Não foi possível identificar que período do ano você nasceu.'
+    puts 'No fue posible identificar en qué época del año naciste.'
 end
