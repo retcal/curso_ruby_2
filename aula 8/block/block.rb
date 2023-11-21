a987f867108ce4ab39cf2516e464b07baa6e4642
retcal <h.retamales@hotmail.com>
2023-11-21 10:58:56 -0300

    Update block.rb

--- aula 8/block/block.rb
+++ aula 8/block/block.rb
@@ -1,6 +1,6 @@
 # Blocks
-# Um bloco pode ser entendido como uma função sem nome.
-# É definido entre do...end ou colchetes e da mesma forma que os métodos pode receber parâmetros para a excução.
+# Un bloque puede entenderse como una función sin nombre.
+# Se define entre do...end o corchetes y, al igual que los métodos, puede recibir parámetros para su ejecución.
 sum = 0
 numbers = [5, 10, 5]
 numbers.each {|number| sum += number}
