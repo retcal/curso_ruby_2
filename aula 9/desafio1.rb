9b85a18587e65d4268abe4336bf94fe9c710d797
retcal <h.retamales@hotmail.com>
2023-11-23 09:36:58 -0300

    Update desafio1.rb

--- aula 9/desafio1.rb
+++ aula 9/desafio1.rb
@@ -1,6 +1,6 @@
-# Missão 1
-# Crie uma expressão regular que faça o casamento de padrão com o número de telefone presente no texto a seguir.
+# Misión 1
+# Crear una expresión regular que coincida con el patrón con el número de teléfono en el siguiente texto.
 
-texto = 'Olá, tudo bem? Meu whats app é (11) 9 4321-1234'
+texto = 'Hola, ¿qué tal? Mi whats app es (11) 9 4321-1234'
 match = /\(\d{2}\) \d \d{4}-\d{4}/.match(texto)
 puts match
