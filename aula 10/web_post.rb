02a6ae79ced6c4200e83902fa07cd0039de5927c
retcal <h.retamales@hotmail.com>
2023-11-23 09:53:01 -0300

    Update web_post.rb

--- aula 10/web_post.rb
+++ aula 10/web_post.rb
@@ -2,8 +2,8 @@
 require 'net/http'
  
 req = Net::HTTP::Post.new("/api/users")
-# para fazer chamadas https
-req.set_form_data({ name:'Mario', job:'Encanador' })
+# para realizar llamadas https
+req.set_form_data({ name:'Mario', job:'Fontanero' })
  
 response = Net::HTTP.start('reqres.in', use_ssl: true) do |http|
  http.request(req)
