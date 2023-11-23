fbab75b67186a23626a90b093b95b31a7c3e0c89
retcal <h.retamales@hotmail.com>
2023-11-23 10:00:41 -0300

    Update classe.rb

--- aula 7/classe.rb
+++ aula 7/classe.rb
@@ -1,14 +1,14 @@
 class User
     @@User_count = 0
     def add(name)
-        puts "User #{name} adicionado"
+        puts "User #{name} añadido"
         @@User_count +=1
         puts @@User_count
     end
 end
 
 first_user = User.new
-first_user.add('joão')
+first_user.add('John')
 
 second_user = User.new
 second_user.add('Mario')
